45b38d1f40410ba5edbf6eb9682433d736e7a692
Mncedisi95 <mncedisimasondo4@gmail.com>
2024-09-03 19:46:26 +0200

    new updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, SimpleChange } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { IndexComponent } from './components/index/index.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { Console } from 'node:console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,IndexComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IndexComponent } from './components/index/index.component';
 
 export const routes: Routes = [
 
     {
       path:'navbar-component', component:NavbarComponent
     },
+    {
+        path:'index', component:IndexComponent
+    }
 ];
